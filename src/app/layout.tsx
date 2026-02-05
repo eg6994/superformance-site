1ce438b1109fcf8ba7c9bb8c7eb8c90a78eaa975
eg6994 <eg6994@hotmail.com>
2026-02-05 08:45:58 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import ClientBody from "./ClientBody";
-import Script from "next/script";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SUPERFORMANCE | Digital Consulting Agency",
-  description: "Our Business is to Grow your Business. Unlock your business's digital success with our expert digital consulting services. We drive growth, enhance strategies, and transform brands for online excellence.",
-  keywords: ["digital marketing", "consulting", "SEO", "digital transformation", "AI solutions", "marketing strategy"],
+  title: "Superformance",
+  description: "Official Site",
 };
 
 export default function RootLayout({
@@ -26,16 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} dark`}>
-      <head>
-        <Script
-          crossOrigin="anonymous"
-          src="//unpkg.com/same-runtime/dist/index.global.js"
-        />
-      </head>
-      <body suppressHydrationWarning className="antialiased">
-        <ClientBody>{children}</ClientBody>
-      </body>
+    <html lang="he" dir="rtl">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
